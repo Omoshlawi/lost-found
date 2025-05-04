ebe68d530f079ae09c329a651fb672a5cc0c6579
Laurent Ouma <lawiomosh3@gmail.com>
2025-05-04 18:13:34 +0300

    Enhance DashboardLayout with modal handling for mobile; refactor component structure, improve workspace management, and streamline layout for better user experience.

--- src/features/dashboard/components/DashboardLayout.tsx
+++ src/features/dashboard/components/DashboardLayout.tsx
@@ -1,6 +1,6 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
-import { ActionIcon, AppShell, Box, Stack } from '@mantine/core';
+import { ActionIcon, AppShell, Box, Modal, Stack } from '@mantine/core';
 import { useDisclosure, useMediaQuery } from '@mantine/hooks';
 import { TablerIcon } from '@/components';
 import { useWorkspace } from '@/components/Workspace';
@@ -11,61 +11,83 @@ const DashboardLayout: FC = () => {
   const [opened, { toggle }] = useDisclosure();
   const { workspace, width } = useWorkspace();
   const isMobile = useMediaQuery('(max-width: 48em)');
+  const [modalOpened, { open: modalOpen, close: modalClose }] = useDisclosure(
+    isMobile && Boolean(workspace?.component)
+  );
+
+  useEffect(() => {
+    if (isMobile && workspace?.component) {
+      modalOpen();
+    } else {
+      modalClose();
+    }
+  }, [isMobile, workspace, modalClose, modalOpen]);
 
   return (
-    <AppShell
-      header={{ height: 60 }}
-      navbar={{
-        width: 300,
-        breakpoint: '50em',
-        collapsed: { mobile: !opened },
-      }}
-      aside={{ collapsed: { mobile: !opened }, breakpoint: '50em', width: width ?? 50 }}
-      footer={isMobile && workspace?.component ? { height: '100vh' } : undefined}
-      styles={{
-        aside: { transition: 'width 300ms ease' },
-        footer: { transition: 'height 300ms ease' },
-      }}
-      padding="md"
-    >
-      <TopNavBar />
-      <AppShell.Navbar>
-        <SideNav />
-      </AppShell.Navbar>
-      <AppShell.Aside>
-        {!workspace && (
-          <Stack align="center" py={'sm'}>
-            <ActionIcon
-              size={35}
-              radius={'50%'}
-              variant="outline"
-              aria-label="ActionIcon with size as a number"
-            >
-              <TablerIcon name="plus" size={24} />
-            </ActionIcon>
-            <ActionIcon
-              size={35}
-              radius={'50%'}
-              variant="outline"
-              aria-label="ActionIcon with size as a number"
-            >
-              <TablerIcon name="bell" size={24} />
-            </ActionIcon>
-          </Stack>
-        )}
-        {workspace?.component}
-      </AppShell.Aside>
-      <AppShell.Main>
-        <Outlet />
-      </AppShell.Main>
-      <AppShell.Footer component={isMobile && workspace?.component ? Box : undefined}>
-        {isMobile && workspace?.component ? (
-          workspace.component
-        ) : (
+    <>
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: '50em',
+          collapsed: { mobile: !opened },
+        }}
+        aside={{ collapsed: { mobile: !opened }, breakpoint: '50em', width: width ?? 50 }}
+        styles={{
+          aside: { transition: 'width 300ms ease' },
+        }}
+        padding="md"
+      >
+        <TopNavBar />
+        <AppShell.Navbar>
+          <SideNav />
+        </AppShell.Navbar>
+        <AppShell.Aside>
+          {!workspace && (
+            <Stack align="center" py={'sm'}>
+              <ActionIcon
+                size={35}
+                radius={'50%'}
+                variant="outline"
+                aria-label="ActionIcon with size as a number"
+              >
+                <TablerIcon name="plus" size={24} />
+              </ActionIcon>
+              <ActionIcon
+                size={35}
+                radius={'50%'}
+                variant="outline"
+                aria-label="ActionIcon with size as a number"
+              >
+                <TablerIcon name="bell" size={24} />
+              </ActionIcon>
+            </Stack>
+          )}
+          {workspace?.component}
+        </AppShell.Aside>
+        <AppShell.Main style={{ position: 'relative' }}>
+          <Outlet />
+        </AppShell.Main>
+        <AppShell.Footer>
           <p>lorem impsum with some interesting staff bana</p>
-        )}
-      </AppShell.Footer>
-    </AppShell>
+        </AppShell.Footer>
+      </AppShell>
+      <Modal
+        opened={modalOpened}
+        onClose={modalClose}
+        fullScreen
+        radius={0}
+        withCloseButton={false}
+        transitionProps={{ transition: 'slide-up', duration: 200 }}
+        p={0}
+        m={0}
+        styles={{
+          body: { padding: 0, width: '100%', height: '100%', flex: 1 },
+        }}
+      >
+        {workspace?.component}
+      </Modal>
+    </>
   );
 };
 
